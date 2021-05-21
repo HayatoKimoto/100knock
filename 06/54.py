748312247fc07a641e6b5e7f5e43383bd22e95d1
木元颯人 <kimotosatsuhito@kimotosatsuhitonoMacBook-Pro.local>
2021-05-21 14:12:43 +0900

    revised folder

--- 06/54.py
+++ 06/54.py
@@ -11,6 +11,7 @@ x_test  = pd.read_pickle('ans50/test.feature.pkl')
 train_df = pd.read_table('ans50/train.tsv',header = None)[0]
 test_df  = pd.read_table('ans50/test.tsv',header = None)[0]
 
+#正解ラベルの生成
 cate2num = {"b": 0, "t": 1, "e": 2, "m": 3}
 y_train = []
 for category in train_df:
@@ -20,9 +21,17 @@ y_test = []
 for category in test_df:
     y_test.append(cate2num[category])
 
-x_train_pred = clf.predict(x_train) 
-x_test_pred  = clf.predict(x_test) 
+#予測した結果
+y_train_pred = clf.predict(x_train) 
+y_test_pred  = clf.predict(x_test) 
 
-print(accuracy_score(y_train,x_train_pred))
-print(accuracy_score(y_test,x_test_pred))
+#学習データと評価データの正解率
+print(accuracy_score(y_train,y_train_pred))
+print(accuracy_score(y_test,y_test_pred))
 
+"""
+[プログラムの結果]
+%python 54.py
+0.9672038980509745
+0.8943028485757122
+"""
