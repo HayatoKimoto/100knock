fdf1564e2367ef157dbc215f253005933d8e7612
木元颯人 <kimotosatsuhito@kimotosatsuhitonoMacBook-Pro.local>
2021-05-21 14:11:40 +0900

    revised folder

--- 06/50.py
+++ 06/50.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 with zipfile.ZipFile('NewsAggregatorDataset.zip') as zf:
   with zf.open("newsCorpora.csv") as myfile:
-     newsCorpora = pd.read_csv(myfile, sep='\t', header=None)
+     newsCorpora = pd.read_table(myfile, header=None)
 
 newsCorpora.columns = ['ID','TITLE','URL','PUBLISHER','CATEGORY','STORY','HOSTNAME','TIMESTAMP']
 
@@ -17,15 +17,39 @@ df = df.sample(frac=1)
 #条件4つ目：データの切り分け
 train, val_test = train_test_split(df, test_size=0.2)
 val, test = train_test_split(val_test, test_size=0.5)
+
 """
 train_test_split()関数でリストを分割
-test_sizeは2つめのリストの大きさを指定
-train_sizeは1つめのリストの大きさを指定
+test_sizeは2つめのリストの大きさの割合を指定
+train_sizeは1つめのリストの大きさの割合を指定
 個数でも割合でも指定可能
 """
+
 #保存
 train.to_csv('ans50/train.tsv', columns = ['CATEGORY','TITLE'], sep='\t',header=False, index=False)
 val.to_csv('ans50/valid.tsv', columns = ['CATEGORY','TITLE'], sep='\t',header=False, index=False)
 test.to_csv('ans50/test.tsv', columns = ['CATEGORY','TITLE'], sep='\t',header=False, index=False)
 
-print(len(train[['CATEGORY','TITLE']]),len(test[['CATEGORY','TITLE']]))
+print("学習データ")
+print(train['CATEGORY'].value_counts())
+
+print("評価データ")
+print(test['CATEGORY'].value_counts())
+
+
+
+"""
+[プログラムの結果]
+学習データ
+b    4476
+e    4264
+t    1214
+m     718
+Name: CATEGORY, dtype: int64
+評価データ
+b    575
+e    507
+t    159
+m     93
+Name: CATEGORY, dtype: int64
+"""
