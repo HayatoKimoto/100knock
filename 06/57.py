940f6282c8d9953155dde4dc46c287f0f40de272
木元颯人 <kimotosatsuhito@kimotosatsuhitonoMacBook-Pro.local>
2021-05-21 14:13:16 +0900

    revised folder

--- 06/57.py
+++ 06/57.py
@@ -9,9 +9,35 @@ cv = pickle.load(open("cv.pkl","rb"))
 
 num2cate = {0:"b", 1: "t", 2: "e",3: "m"}
 names = np.array(cv.get_feature_names())
-#インスタンス名.coef_とすることで、パラメータ（重み）を取得することができます
+
+#インスタンス名.coef_とすることで、パラメータ（重み）を取得することができる
 for c,coef in zip(clf.classes_,clf.coef_):
     print(num2cate[c])
     idx = np.argsort(coef)[::-1]
     print('重みの高い特徴量トップ10:',names[idx][:10])
     print('重みの低い特徴量トップ10:',names[idx][-10:][::-1])
+
+"""
+[プログラムの結果]
+b
+重みの高い特徴量トップ10: ['bank' 'fed' 'ecb' 'euro' 'oil' 'ukraine' 'china' 'stocks' 'yellen'
+ 'dollar']
+重みの低い特徴量トップ10: ['ebola' 'she' 'video' 'aereo' 'her' 'virus' 'star' 'kardashian' 'drug'
+ 'fda']
+t
+重みの高い特徴量トップ10: ['google' 'facebook' 'apple' 'microsoft' 'climate' 'nasa' 'tesla' 'fcc'
+ 'gm' 'heartbleed']
+重みの低い特徴量トップ10: ['stocks' 'american' 'her' 'percent' 'fed' 'drug' 'cancer' 'still' 'fda'
+ 'ecb']
+e
+重みの高い特徴量トップ10: ['kardashian' 'chris' 'movie' 'miley' 'film' 'she' 'paul' 'cyrus' 'jay'
+ 'thrones']
+重みの低い特徴量トップ10: ['google' 'study' 'billion' 'china' 'gm' 'ceo' 'facebook' 'risk'
+ 'microsoft' 'apple']
+m
+重みの高い特徴量トップ10: ['ebola' 'cancer' 'fda' 'drug' 'study' 'mers' 'health' 'cases' 'cdc'
+ 'heart']
+重みの低い特徴量トップ10: ['gm' 'facebook' 'apple' 'bank' 'google' 'ceo' 'buy' 'twitter' 'deal'
+ 'climate']
+
+ """
