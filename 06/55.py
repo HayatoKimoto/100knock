6e6f0f235f838a2031faefa43239e51179df2db1
木元颯人 <kimotosatsuhito@kimotosatsuhitonoMacBook-Pro.local>
2021-05-21 14:12:54 +0900

    revised folder

--- 06/55.py
+++ 06/55.py
@@ -12,6 +12,7 @@ x_test  = pd.read_pickle('ans50/test.feature.pkl')
 train_df = pd.read_table('ans50/train.tsv',header = None)[0]
 test_df  = pd.read_table('ans50/test.tsv',header = None)[0]
 
+#正解ラベルの生成
 cate2num = {"b": 0, "t": 1, "e": 2, "m": 3}
 y_train = []
 for category in train_df:
@@ -21,8 +22,22 @@ y_test = []
 for category in test_df:
     y_test.append(cate2num[category])
 
-x_train_pred = clf.predict(x_train) 
-x_test_pred  = clf.predict(x_test) 
+#予測した結果
+y_train_pred = clf.predict(x_train) 
+y_test_pred  = clf.predict(x_test) 
 
-print(confusion_matrix(y_train,x_train_pred))
-print(confusion_matrix(y_test,x_test_pred))
+
+print(confusion_matrix(y_train,y_train_pred))
+print(confusion_matrix(y_test,y_test_pred))
+
+"""
+%python 55.py
+[[4408   40   36    8]
+ [  90 1089   50    2]
+ [  27    9 4179    0]
+ [  42    4   42  646]]
+[[530   9  23   5]
+ [ 32 106  17   0]
+ [ 14   2 504   1]
+ [ 10   6  22  53]]
+"""
