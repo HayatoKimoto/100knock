37b2df13d07008c3fdda552d35a36597ab8c3a30
HayatoKimoto <k1810220@gl.cc.uec.ac.jp>
2021-06-17 23:47:19 +0900

    revised folder

--- 08/73.py
+++ 08/73.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
