bf9409983a2e36c18a5cb38e5a76ce2055a2c01d
HayatoKimoto <k1810220@gl.cc.uec.ac.jp>
2021-07-02 14:23:08 +0900

    revised folder

--- 10/92.py
+++ 10/92.py
@@ -147,4 +147,10 @@ V_en = get_vocab_en()
 model = MyTransformer(device=device, max_len=max_len, source_vocab_length=V_ja, target_vocab_length=V_en).to(device)
 model.load_state_dict(torch.load('best_model.pt'))
 
-print(greeedy_decode_sentence(model,"私は毎日ゲームをします。"))
+print(greeedy_decode_sentence(model , "私は毎日ゲームをします。"))
+
+"""
+[プログラムの結果]
+%python 92.py
+ I will be able to Koui the first day and Koui Koui Koui .
+"""
