a0e8ada7bb6c66e3443ca568f58b8810aafa2e4b
HayatoKimoto <k1810220@gl.cc.uec.ac.jp>
2021-07-09 12:16:02 +0900

    revised folder

--- 10/95.py
+++ 10/95.py
@@ -1,16 +1,68 @@
-import sys
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.utils.data import DataLoader,TensorDataset
-from torch.nn.utils.rnn import pack_sequence, pad_packed_sequence
-from word2id import get_id_ja, get_vocab_ja
-from word2id import get_id_en, get_vocab_en
-import torch.tensor as Tensor
-from torch.nn.init import xavier_uniform_
-import math
-from tqdm import tqdm
-import MeCab
-import pickle
-import nltk
+import sentencepiece as spm
+
+
+spm.SentencePieceTrainer.Train(
+    '--input=kftt-data-1.0/data/tok/kyoto-train.cln.ja, --model_prefix=sentencepiece-kftt-16000-ja --model_type=word --character_coverage=0.9995 --vocab_size=16000 --pad_id=0 --unk_id=3'
+)
+
+
+spm.SentencePieceTrainer.Train(
+    '--input=kftt-data-1.0/data/tok/kyoto-train.cln.en, --model_prefix=sentencepiece-kftt-16000-en --model_type=word --character_coverage=1.0 --vocab_size=16000 --pad_id=0 --unk_id=3'
+)
+
+
+
+
+
+#日本語
+sp = spm.SentencePieceProcessor()
+sp.Load("sentencepiece-kftt-16000-ja.model")
+
+with open('kftt-data-1.0/data/tok/kyoto-train.cln.ja') as f:
+    lines=f.readlines()
+
+line=lines[0]
+print(line)
+#日本 の 水墨 画 を 一変 さ せ た 。
+tokens=line.split()
+print(tokens)
+#['日本', 'の', '水墨', '画', 'を', '一変', 'さ', 'せ', 'た', '。']
+print(sp.DecodePieces(tokens))
+#日本の水墨画を一変させた。
+
+ids=sp.EncodeAsIds(line)
+print(ids)
+#[42, 4, 6446, 510, 11, 56, 4341, 26, 117, 10, 5]
+print(sp.DecodeIds(ids))
+#日本 の 水墨 画 を 一変 さ せ た 。
+print(sp.GetPieceSize())
+#16000
+
+print(sp.IdToPiece(10))
+#1
+
+
+#英語
+sp = spm.SentencePieceProcessor()
+sp.Load("sentencepiece-kftt-16000-en.model")
+
+with open('kftt-data-1.0/data/tok/kyoto-train.cln.en') as f:
+    lines=f.readlines()
+
+line=lines[0]
+print(line)
+#He revolutionized the Japanese ink painting .
+
+tokens=line.split()
+print(tokens)
+#['He', 'revolutionized', 'the', 'Japanese', 'ink', 'painting', '.']
+print(sp.DecodePieces(tokens))
+#HerevolutionizedtheJapaneseinkpainting.
+ids=sp.EncodeAsIds(line)
+print(ids)
+#[31, 0, 4, 60, 1576, 540, 6]
+print(sp.DecodeIds(ids))
+#He ⁇  the Japanese ink painting .
+print(sp.GetPieceSize())
+#16000
+print(sp.IdToPiece(10))
